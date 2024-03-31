cffcc357862290c66c97e9d228a51f5c5394a830
“krv006” <rvkamronbek@gmail.com>
2024-04-01 00:33:40 +0500

    add router.py and serializers.py

---
+++ apps/router.py
@@ -0,0 +1,40 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+
+    path('categories/', CategoryListAPIView.as_view(), name='categories-list'),
+    path('categories/<int:pk>/', CategoryRetrieveUpdateDestroyAPIView.as_view(), name='categories-detail'),
+
+    path('card/', CardListAPIView.as_view(), name='card-list'),
+    path('card/<int:pk>/', CardRetrieveUpdateDestroyAPIView.as_view(), name='card-detail'),
+
+    path('sub_category/', Sub_categoryListAPIView.as_view(), name='sub_category-list'),
+    path('sub_category/<int:pk>/', Sub_categoryRetrieveUpdateDestroyAPIView.as_view(), name='sub_category-detail'),
+
+    path('consumption/', ConsumptionListAPIView.as_view(), name='consumption-list'),
+    path('consumption/<int:pk>/', ConsumptionRetrieveUpdateDestroyAPIView.as_view(), name='consumption-detail'),
+
+    path('plastic_card/', Plastic_cardListAPIView.as_view(), name='plastic_card-list'),
+    path('plastic_card/<int:pk>/', Plastic_cardRetrieveUpdateDestroyAPIView.as_view(), name='plastic_card-detail'),
+
+    path('product/', ProductListAPIView.as_view(), name='product-list'),
+    path('product/<int:pk>/', ProductRetrieveUpdateDestroyAPIView.as_view(), name='product-detail'),
+    # path('productdelete/<int:pk>/', ProductRetrieveUpdateDestroyAPIView.as_view(), name='product-detail'),
+
+
+    path('debtors/', DebtorsListAPIView.as_view(), name='debtors-list'),
+    path('debtors/<int:pk>/', DebtorsRetrieveUpdateDestroyAPIView.as_view(), name='debtors-detail'),
+
+    path('workers/', WorkersListAPIView.as_view(), name='workers-list'),
+    path('workers/<int:pk>/', WorkersRetrieveUpdateDestroyAPIView.as_view(), name='workers-detail'),
+
+    path('dollars/', DollarsListAPIView.as_view(), name='dollars-list'),
+    path('dollars/<int:pk>/', DollarsRetrieveUpdateDestroyAPIView.as_view(), name='dollars-detail'),
+
+    path('camunalca/', CamunalcaListAPIView.as_view(), name='camunalca-list'),
+    path('camunalca/<int:pk>/', CamunalcaRetrieveUpdateDestroyAPIView.as_view(), name='camunalca-detail'),
+]
+
+# statistika oy xafta yil oyliklar qilish rasxodlar 
+# rasxodlar 
---
+++ apps/serializers.py
@@ -0,0 +1,76 @@
+from rest_framework import serializers
+from .models import Category, Card, Consumption, Sub_category, Plastic_card, Product, Debtors, Workers, Camunalca, Dollars
+
+class Sub_categorySerializer(serializers.ModelSerializer):
+
+    class Meta :
+        model = Sub_category
+        fields = ['name']
+
+class CategorySerializer(serializers.ModelSerializer):
+    
+    sub_category = Sub_categorySerializer(many=True)
+    
+    class Meta :
+        model = Category
+        fields = ['name', 'info', 'sub_category']
+
+
+class CardSerializer(serializers.ModelSerializer):
+
+    class Meta :
+        model = Card
+        fields = ['plastic_name']
+
+
+class ConsumptionSerializer(serializers.ModelSerializer):
+    sub_category_name = serializers.ReadOnlyField(source='sub_category.name') 
+    status_display = serializers.CharField(source='get_status_display') 
+    # 1 chisida nomini olish uchun ReadOnlyField ishlatildi faqat oqish uchun
+    # 2 chisida CharField status nomini olish uchun ishlatildi
+
+    class Meta :
+        model = Consumption
+        fields = ['name', 'info', 'cost', 'sub_category_name', 'time', 'status_display', 'date']
+
+
+class Plastic_cardSerializer(serializers.ModelSerializer):
+
+    class Meta :
+        model = Plastic_card
+        fields = ['plastic_name', 'cost']
+
+class ProductSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta :
+        model = Product
+        fields = ['name', 'quantity', 'price', 'user']
+
+class DebtorsSerializer(serializers.ModelSerializer):
+
+    class Meta :
+        model = Debtors
+        fields = ['full_name', 'phone_numuber', 'product', 'date', 'price']
+    
+
+class WorkersSerializer(serializers.ModelSerializer):
+
+    class Meta :
+        model = Workers
+        fields = ['workers_salary_month', 'workers_salary_weeks', 'workers_salary_days', 'workers_salary_oclocs']
+
+
+class CamunalcaSerializer(serializers.ModelSerializer):
+
+    class Meta :
+        model = Camunalca
+        fields = ['water', 'gas', 'electricity', 'wifi', 'lands' ]
+    
+
+
+class DollarsSerializer(serializers.ModelSerializer):
+
+    class Meta :
+        model = Dollars
+        fields = ['dollar', 'dollar_course']
