eba92fa91ead7b0a337c3180af2e8d4c6ab5630f
“krv006” <rvkamronbek@gmail.com>
2024-04-01 00:31:59 +0500

    add project models.py and views.py

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,114 @@
 from django.db import models
+from django.db import models as m
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Sub_category(models.Model):
+    name = models.CharField(max_length = 250)
+    def __str__(self):
+        return self.name 
+
+
+
+class Consumption(models.Model):
+    CONSUPTION_CHOISES_FIELD = [
+        (1, 'not_gived'),
+        (2, 'paid'),
+        (3, 'not_paid'),
+        (4, 'pending'),
+    ]
+    name = models.CharField(max_length = 250)
+    info = models.TextField(null = True)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField( auto_now_add = True )
+    time = models.TimeField(auto_now_add = False, null=True)
+    status = models.IntegerField(choices=CONSUPTION_CHOISES_FIELD, default=1)
+    sub_category = models.ForeignKey(Sub_category,on_delete = models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length = 250)
+    info = models.TextField()    
+    sub_category = models.ManyToManyField(Sub_category)
+    
+    def __str__(self):
+        return self.name
+
+
+
+class Card(models.Model):
+    plastic_name = models.CharField(max_length = 250)
+    
+    def __str__(self):
+        return self.plastic_name 
+
+
+class Plastic_card(models.Model):
+    cost = models.IntegerField()
+    plastic_name = models.ForeignKey(Card,on_delete = models.CASCADE )
+    
+    def __int__(self):
+        return self.cost
+
+
+class Product(m.Model):
+    name = m.CharField(max_length=250)
+    quantity = m.PositiveSmallIntegerField()
+    price = m.DecimalField(max_digits=11, decimal_places=3)
+    user = m.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+class Debtors(m.Model):
+    full_name = m.CharField(max_length=250)
+    phone_numuber = m.CharField(max_length=250, blank = True)
+    product = m.ManyToManyField(Product)
+    date = m.DateField(auto_now_add=True)
+    price = m.DecimalField(max_digits=11, decimal_places=3)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Dollars(m.Model):
+    dollar = m.IntegerField()
+    dollar_course = m.IntegerField()
+
+    def __int__(self):
+        return self.dollar
+    
+
+# class Camunalca(m.Model):
+#     water = m.IntegerField()
+#     gas = m.IntegerField()
+#     electricity = m.IntegerField()
+#     wifi = m.IntegerField()
+#     lands = m.IntegerField()    # lands soliqlar
+    
+#     def __int__(self):
+#         return self.water
+
+class Workers(m.Model):
+    workers_salary_month = m.IntegerField()
+    workers_salary_weeks = m.IntegerField()
+    workers_salary_days = m.IntegerField()
+    workers_salary_oclocs = m.IntegerField()
+
+    def __int__(self):
+        return self.workers_salary_month
+    
+
+class Department(m.Model):
+    name = m.CharField(max_length = 50)
+    is_active = m.BooleanField(default = True)
+
+class Employee(m.Model):
+    name = m.CharField(max_length = 100)
+    department = m.ForeignKey(Department, on_delete = models.CASCADE, limit_choices_to = { 'is_active' : True})
--- apps/views.py
+++ apps/views.py
@@ -1,3 +1,114 @@
 from django.shortcuts import render
+from rest_framework.response import Response 
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Category, Card, Sub_category, Consumption, Plastic_card, Product, Debtors, Workers, Dollars, Camunalca
+from .serializers import CategorySerializer, CardSerializer, Sub_categorySerializer, ConsumptionSerializer
+from .serializers import Plastic_cardSerializer, ProductSerializer, DebtorsSerializer 
+from .serializers import WorkersSerializer, DollarsSerializer, CamunalcaSerializer
 
-# Create your views here.
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework import generics, viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet
+
+from .permissions import IsAdminOrReadOnly
+
+class Sub_categoryListAPIView(ListCreateAPIView):
+    queryset = Sub_category.objects.all()
+    serializer_class = Sub_categorySerializer
+
+class Sub_categoryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Sub_category.objects.all()
+    serializer_class = Sub_categorySerializer
+
+
+class ConsumptionListAPIView(ListCreateAPIView):
+    queryset = Consumption.objects.all()
+    serializer_class = ConsumptionSerializer
+
+class ConsumptionRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Consumption.objects.all()
+    serializer_class = ConsumptionSerializer
+    
+class CategoryListAPIView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+
+class CardListAPIView(ListCreateAPIView):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+
+class CardRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Card.objects.all()
+    serializer_class = CardSerializer
+
+
+
+class Plastic_cardListAPIView(ListCreateAPIView):
+    queryset = Plastic_card.objects.all()
+    serializer_class = Plastic_cardSerializer
+
+class Plastic_cardRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Plastic_card.objects.all()
+    serializer_class = Plastic_cardSerializer
+
+
+class ProductListAPIView(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+
+class ProductRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = (IsAdminOrReadOnly, )
+
+class ProductRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = (IsAdminOrReadOnly, )
+    
+
+
+class DebtorsListAPIView(ListCreateAPIView):
+    queryset = Debtors.objects.all()
+    serializer_class = DebtorsSerializer
+
+class DebtorsRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Debtors.objects.all()
+    serializer_class = DebtorsSerializer
+
+class WorkersListAPIView(ListCreateAPIView):
+    queryset = Workers.objects.all()
+    serializer_class = WorkersSerializer
+
+class WorkersRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Workers.objects.all()
+    serializer_class = WorkersSerializer
+
+
+class DollarsListAPIView(ListCreateAPIView):
+    queryset = Dollars.objects.all()
+    serializer_class = DollarsSerializer
+
+class DollarsRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Dollars.objects.all()
+    serializer_class = DollarsSerializer
+
+class CamunalcaListAPIView(ListCreateAPIView):
+    queryset = Camunalca.objects.all()
+    serializer_class = CamunalcaSerializer
+
+class CamunalcaRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Camunalca.objects.all()
+    serializer_class = CamunalcaSerializer
